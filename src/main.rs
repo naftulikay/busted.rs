1638d5dcfb471c41569b43d69f6f5ff80733af21
Naftuli Kay <me@naftuli.wtf>
2018-04-03 15:51:56 -0700

    [WIP] Template Rendering Engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#[macro_use]
+extern crate structopt;
+
+use structopt::StructOpt;
+
+#[derive(Debug,StructOpt)]
+#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+/// A collection of utilities written in Rust.
+struct Opt {
+    /// Enable verbose application logging.
+    #[structopt(short="v", long="verbose")]
+    verbose: bool,
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug,StructOpt)]
+enum Command {
+    /// Render a Jinja/Askama template.
+    #[structopt(name = "render")]
+    Render {
+        acorns: u32
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
